acda00dc0226a17e11f21507f19bf1301b3df19e
Tsanaativa Vinnera <tsanaativa.vinnera@ui.ac.id>
2021-03-20 14:42:05 +0700

    OS211 tsanaativa

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
-REC2="cbk@dummy"
+REC2="tsanaativa.vinnera@ui.ac.id"
 REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
